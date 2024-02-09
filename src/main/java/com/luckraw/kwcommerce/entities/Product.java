08a3cb60576f474e08cf5cc4e5c1e4c45863b18c
Lucas <krawdev@gmail.com>
2024-02-09 12:41:34 -0300

    feat: Order and Product getters.

--- src/main/java/com/luckraw/kwcommerce/entities/Product.java
+++ src/main/java/com/luckraw/kwcommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.luckraw.kwcommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -80,5 +81,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
