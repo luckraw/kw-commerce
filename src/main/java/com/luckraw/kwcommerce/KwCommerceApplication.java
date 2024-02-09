e215ffeee6e321c1fd425b10298215b57880db21
Lucas <krawdev@gmail.com>
2024-02-08 23:49:25 -0300

    Initial commit

---
+++ src/main/java/com/luckraw/kwcommerce/KwCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.kwcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckraw/kwcommerce/KwCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.kwcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
